deec9be7a8d5249bea0c315e49dd5377f7bacd23
martinaalfred <martinaalfred487@gmail.com>
2026-05-11 17:42:18 +0300

    Add Kubernetes monitoring with Prometheus and Grafana

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 window.API_CONFIG = {
-  TICKET: "http://127.0.0.1:5000",
-  SUPPORT: "http://127.0.0.1:5001",
-  NOTIF: "http://127.0.0.1:5002",
-  REPORT: "http://127.0.0.1:5003"
+  TICKET: "http://127.0.0.1:5011",
+  SUPPORT: "http://127.0.0.1:5010",
+  NOTIF: "http://127.0.0.1:5012",
+  REPORT: "http://127.0.0.1:5013"
 };
